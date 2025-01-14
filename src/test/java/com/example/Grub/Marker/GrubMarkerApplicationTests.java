315bbe33c36786e493afebee637f9e1ea181b005
Mobo <princemoronfolu@gmail.com>
2025-01-13 18:26:41 -0600

    initial commit
    initial commit

---
+++ src/test/java/com/example/Grub/Marker/GrubMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Grub.Marker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrubMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
